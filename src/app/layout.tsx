ad3a20130e394c0036b49ed3eafab84a3a12e11c
Dongjun Hahm <dhahm@ucsc.edu>
2024-10-05 16:30:52 -0700

    Revert "working google login, wip GPT integration and text input box"
    This reverts commit b477a171db0cbcb6fd94c70f926ff5e22d37fe50.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,16 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-import { GeistSans } from "geist/font/sans";
-import { GeistMono } from "geist/font/mono";
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,7 +25,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
