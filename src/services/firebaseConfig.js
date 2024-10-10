4c961e9456feb49f399e62e2b1f609e4f1702ed7
Dongjun Hahm <dhahm@ucsc.edu>
2024-10-10 12:53:18 -0700

    fixed config file to read .env values properly

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -2,18 +2,17 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FB_APIKEY,
-  authDomain: process.env.REACT_APP_FB_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_FB_PROJECTID,
-  storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FB_MESSAGING_SENDERID,
-  appId: process.env.REACT_APP_FB_APPID,
-  measurementId: process.env.REACT_APP_FB_MEASUREMENT_ID,
+  apiKey: process.env.NEXT_PUBLIC_FB_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_FB_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FB_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FB_MESSAGING_SENDERID,
+  appId: process.env.NEXT_PUBLIC_FB_APPID,
+  measurementId: process.env.NEXT_PUBLIC_FB_MEASUREMENT_ID,
 };
-console.log(firebaseConfig);
 
 // Initialize Firebase
 
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig, "final");
 export const auth = getAuth(app);
 export default app;
