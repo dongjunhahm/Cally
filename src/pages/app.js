ec3000085625aee4004a6d549fdb13593ff93924
Dongjun Hahm <dhahm@ucsc.edu>
2024-10-13 21:27:12 -0700

    hoping and praying

--- src/pages/app.js
+++
@@ -1,8 +0,0 @@
-// ./src/pages/_app.js (for JavaScript)
-import "../styles/globals.css";
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
